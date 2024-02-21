2842eb3b3298d922eaceea0856a58e62cfb5b51b
Bryan Bernigen <bryanbernigen@gmail.com>
2024-02-21 08:24:53 +0700

    fix: add cross cut image

--- oilrigadmin/src/components/map/overviewModal.jsx
+++ oilrigadmin/src/components/map/overviewModal.jsx
@@ -97,7 +97,7 @@ export const CrossCutModal = ({
                             <AddMarker />
                             {marker && <Marker position={marker} icon={icon} />}
                             <ImageOverlay
-                                url="https://lh3.google.com/u/0/d/"
+                                url="https://lh3.google.com/u/0/d/1Ok3pbMZhRWth9gaxeh4go8xSDhZF_Srw"
                                 bounds={[
                                     [0, 0],
                                     [3308 / 36, 2338 / 9],
